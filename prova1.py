c9c1ff491d67c4f82847e376fd61e57d268aa380
pbpolete7 <pfornes21208@iesjoanramis.org>
2025-11-12 13:23:24 +0100

    1

---
+++ prova1.py
@@ -0,0 +1,5 @@
+a= int(input("Escriure el primer nombre:"))
+b= int(input("Escriure el segon nombre:"))
+d= int(input("Escriure el tercer nombre:"))
+c= a + b + d
+print("El resultat de la suma {} + {} + {} és {}".format(a, b, d, c))
