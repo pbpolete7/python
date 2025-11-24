cf5ee2d49a2087bae0024f0b189bb0cabb60025e
pbpolete7 <pfornes21208@iesjoanramis.org>
2025-11-24 09:44:00 +0100

    Ex17.py

---
+++ Ex17.py
@@ -0,0 +1,10 @@
+def ex17(a):
+    return len(a)
+
+# Programa principal
+a=[1, 3, 4, "Pere", [3,4],"a", [1, [3,4],5]]
+print(ex17(a))
+b = "Som en Pol, i tu que tal estàs?"
+print(ex17(b))
+c=[]
+print(ex17(c))
