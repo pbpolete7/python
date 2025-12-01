fdc9774bd32a30ab884a5709e504d3375927b11a
pbpolete7 <pfornes21208@iesjoanramis.org>
2025-12-01 09:26:30 +0100

    prova7.py

--- prova7.py
+++ prova7.py
@@ -1,5 +1,5 @@
 v1 = 10
-while ((v1>=1 and v1<=1000) and (v1%2==1)):
+while ((v1>=1 and v1<=1000) and (v1%2==0) and(v1%4!=0)) 
     v1 = int(input("Introdueix l'operador:"))
     print(v1)
 print("Has inserit un número menor o igual que 1, adéu!")
