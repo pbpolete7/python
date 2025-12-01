8fa26792ba460ee380c7bb043d099fe7d825e5e2
pbpolete7 <pfornes21208@iesjoanramis.org>
2025-12-01 09:21:02 +0100

    prova7.py

--- prova7.py
+++ prova7.py
@@ -1,10 +1,15 @@
 v1 = 10
+while ((v1>=1 and v1<=1000) and (v1%2==1)):
+    v1 = int(input("Introdueix l'operador:"))
+    print(v1)
+print("Has inserit un número menor o igual que 1, adéu!")
+"""
+Docstring for prova7
+v1 = 10
 while(v1>=5 and v1<=10) or (v1>=15 and v1<=20) or (v1>=25 and v1<=30) and(v1!=6) and(v1!=16) and(v1!=26):
     v1 = int(input("Introdueix l'operador:"))
     print(v1)
-print("Has inserit un número menor o igual que 5 o 15.")    
-
-"""
+print("Has inserit un número menor o igual que 5 o 15.")
 v1 = int(input("Introdueix el primer operador"))
 v2 = int(input("Introdueix el segon operador"))
 r = v1 == v2
