d059665e5c237ac526fcb59e6c3000bdf842a9e8
pbpolete7 <pfornes21208@iesjoanramis.org>
2025-12-10 13:15:26 +0100

    Ex54.py

---
+++ Ex54.py
@@ -0,0 +1,7 @@
+# Opció 1: utilitzant range amb pas negatiu
+for i in range(10, 0, -1):
+    print(i)
+
+# Opció 2: utilitzant reversed sobre una llista
+for i in reversed(range(1, 11)):
+    print(i)
